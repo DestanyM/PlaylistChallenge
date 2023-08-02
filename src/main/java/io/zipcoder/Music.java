74c4eb6c26ea6352eae269e3199d2004132ad42e
DestanyM <destanymcclellan22@gmail.com>
2023-08-02 00:50:41 -0400

    Completed Music Class

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -3,12 +3,45 @@ package io.zipcoder;
 public class Music {
 
     private String[] playList;
+    private Integer currentIndex;
 
     public Music(String[] playList){
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int nextSong = 0;
+        int previousSong = 0;
+        this.currentIndex = startIndex;
+        while(!playList[currentIndex].equals(selection)) {
+            up();
+            nextSong++;
+        }
+        this.currentIndex = startIndex;
+        while(!playList[currentIndex].equals(selection)) {
+            down();
+            previousSong++;
+        }
+        if(previousSong<nextSong){
+            return previousSong;
+        }
+        return nextSong;
+    }
+
+    private void up() {
+        if (this.currentIndex >= playList.length) {
+            this.currentIndex = 0;
+        } else {
+            this.currentIndex++;
+
+        }
+    }
+
+    public void down() {
+        if (this.currentIndex == 0) {
+            this.currentIndex = playList.length - 1;
+        } else {
+            this.currentIndex--;
+        }
     }
 }
